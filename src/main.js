30d135b352dc271966470ed720b52c7e4ab7570b
Samuel Breznjak <samuelbreznjak35@gmail.com>
2025-01-24 12:49:33 +0100

    added js for code blocks

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-import "./cs16.css";
+document.querySelectorAll(".toggle-vision").forEach((button) => {
+  button.addEventListener("click", function () {
+    const codeBlock = this.closest(".code-block").querySelector(".inner");
+    codeBlock.classList.toggle("hidden");
+    this.textContent = codeBlock.classList.contains("hidden")
+      ? "Show code"
+      : "Hide code";
+  });
+});
+
+document.querySelectorAll(".inner .cs-btn").forEach((button) => {
+  button.addEventListener("click", function () {
+    const codeElement = this.closest(".inner").querySelector("code");
+    const codeToCopy = codeElement.textContent;
+    navigator.clipboard
+      .writeText(codeToCopy)
+      .then(() => {
+        this.textContent = "Copied!";
+        setTimeout(() => {
+          this.textContent = "Copy";
+        }, 2000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy code: ", err);
+      });
+  });
+});
